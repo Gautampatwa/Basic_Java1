84901c6f926d30eec997daeafa0b36955fa9cbee
gautampatwa500@gmail.com <Bholenath@123654>
2025-08-17 23:33:13 +0530

    Anagram Groups

--- src/StringExample/GroupAnagrams.java
+++ src/StringExample/GroupAnagrams.java
@@ -9,16 +9,16 @@ public class GroupAnagrams {
     }
 
     public static List<List<String>> groupAnagrams(String[] strs) {
-        Map<String, List<String>> anagramGroups = new HashMap<>();
+        Map<String, List<String>> map = new HashMap<>();
         for (String str : strs) {
             char[] charArray = str.toCharArray();
             Arrays.sort(charArray);
             String sortedKey = new String(charArray);
-            if (!anagramGroups.containsKey(sortedKey)) {
-                anagramGroups.put(sortedKey, new LinkedList<>());
+            if (!map.containsKey(sortedKey)) {
+                map.put(sortedKey, new LinkedList<>());
             }
-            anagramGroups.get(sortedKey).add(str);
+            map.get(sortedKey).add(str);
         }
-        return new ArrayList<>(anagramGroups.values());
+        return new ArrayList<>(map.values());
     }
 }
